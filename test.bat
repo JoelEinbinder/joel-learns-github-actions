1e44235ef41b5fd850b4d30a91758e30429b9ce6
Joel Einbinder <joel.einbinder@gmail.com>
2020-02-17 20:09:14 +0000

    dumpio on

--- test.bat
+++ test.bat
@@ -2,6 +2,7 @@
 setLocal EnableDelayedExpansion
 git clone https://github.com/microsoft/playwright.git
 set WKPATH=%cd%\bin\MiniBrowser.exe
+set DUMPIO=true
 cd playwright
 call npm i
 call npm run wtest
